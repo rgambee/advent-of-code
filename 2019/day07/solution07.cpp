86b48e4e3e2f24e76810749a03803320787df4dd
Robert Gambee <robertgambee@gmail.com>
2019-12-08 21:15:27 -0500

    Add solution for day 7, part 2

--- 2019/day07/solution07.cpp
+++ 2019/day07/solution07.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
 #include <array>
+#include <chrono>
+#include <future>
 #include <limits>
-#include <sstream>
+#include <thread>
 
 #include "utils.h"
 
@@ -11,15 +13,35 @@ using phase_settings_type = std::array<int, 5>;
 
 int simulate_phase_settings(std::vector<int> program,
                             const phase_settings_type &phase_settings) {
-    int amp_output = 0;
-    for (auto setting: phase_settings) {
-        std::stringstream program_input;
-        std::stringstream program_output;
-        program_input << setting << " " << amp_output;
-        run_intcode_program(program, program_input, program_output);
-        program_output >> amp_output;
+    std::vector<std::promise<int> > promises(phase_settings.size());
+    std::vector<std::future<int> > async_futures;
+    for (phase_settings_type::size_type i = 0; i < phase_settings.size(); ++i) {
+        promises[i].set_value(phase_settings[i]);
+
+        auto get_input = [&promises, i]() -> int {
+            auto input = promises[i].get_future().get();
+            // Reset promise so it can be used later
+            promises[i] = std::promise<int>();
+            return input;
+        };
+
+        auto give_output = [&promises, i](int output) -> void {
+            auto ind = (i + 1) % promises.size();
+            promises[ind].set_value(output);
+        };
+
+        async_futures.push_back(std::move(std::async(
+            std::launch::async,
+            // Wrap run_intcode_program in lambda so overload resolution works
+            [program, get_input, give_output]() -> int {
+                return run_intcode_program(program, get_input, give_output);
+            })));
     }
-    return amp_output;
+    // Wait a bit for the first thread to be ready for the next input
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    promises[0].set_value(0);
+    async_futures[phase_settings.size() - 1].wait();
+    return promises[0].get_future().get();
 }
 
 
@@ -27,19 +49,27 @@ int main(int argc, char **argv) {
     auto input_stream = open_input_file(argc, argv);
     auto program = load_intcode_program(input_stream);
 
-    std::array<int, 5> phase_settings{0, 1, 2, 3, 4};
-    auto max_signal = std::numeric_limits<int>::min();
+    std::cout << "Calculating part 1..." << std::endl;
+    phase_settings_type phase_settings{0, 1, 2, 3, 4};
+    auto part1_max = std::numeric_limits<int>::min();
     do {
         auto signal = simulate_phase_settings(program, phase_settings);
-        if (signal > max_signal) {
-            max_signal = signal;
-        }
+        part1_max = std::max(part1_max, signal);
+    } while (std::next_permutation(phase_settings.begin(), phase_settings.end()));
+
+    std::cout << "Calculating part 2..." << std::endl;
+    phase_settings = phase_settings_type{5, 6, 7, 8, 9};
+    auto part2_max = std::numeric_limits<int>::min();
+    do {
+        auto signal = simulate_phase_settings(program, phase_settings);
+        part2_max = std::max(part2_max, signal);
     } while (std::next_permutation(phase_settings.begin(), phase_settings.end()));
 
 
     std::cout << "PART 1" << std::endl;
-    std::cout << "Max thruster signal: " << max_signal << std::endl;
+    std::cout << "Max thruster signal: " << part1_max << std::endl;
     std::cout << std::endl;
     std::cout << "PART 2" << std::endl;
+    std::cout << "Max thruster signal: " << part2_max << std::endl;
     return 0;
 }
