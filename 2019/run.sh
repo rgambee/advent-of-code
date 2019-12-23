756546e8d970ecb6495faba24598c48eb4da8add
Robert Gambee <robertgambee@gmail.com>
2019-12-23 08:02:02 -0500

    Remove quotes so wildcard gets expanded
    How did this every work before?

--- 2019/run.sh
+++ 2019/run.sh
@@ -13,5 +13,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 DAY="$(printf "%02d" "$1")"
 g++ -std=c++17 -Wall -Wextra -Werror -o "./day$DAY/solution$DAY.exe" -I "./utils/" \
-    "./day$DAY/solution$DAY.cpp" "./utils/*.cpp"
+    "./day$DAY/solution$DAY.cpp" ./utils/*.cpp
 "./day$DAY/solution$DAY.exe" "./day$DAY/input$DAY.txt"
