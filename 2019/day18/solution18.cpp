fe3d247c6243493c1636b54206060f42cd923324
Robert Gambee <robertgambee@gmail.com>
2019-12-29 18:49:36 -0500

    Start solution for day 18

---
+++ 2019/day18/solution18.cpp
@@ -0,0 +1,92 @@
+#include <array>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <set>
+
+#include "utils.h"
+
+
+using coord_type = std::array<int, 2>;
+using key_type = char;
+
+
+enum class TileType {
+    FLOOR = '.',
+    WALL = '#',
+    START = '@',
+    DOOR,
+    KEY
+}
+
+
+bool is_door(char c) {
+    // Doors are upper-case letters
+    return 'A' <= c && c <= 'Z';
+}
+
+
+bool is_key(char c) {
+    // Keys are lower-case letters
+    return 'a' <= c && c <= 'z';
+}
+
+
+char key_to_door(char k) {
+    if (!is_key(k)) {
+        std::cerr << "Not a key: " << k << std::endl;
+        exit(3);
+    }
+    return k + ('A' - 'a');
+}
+
+
+char door_to_key(char d) {
+    if (!is_door(d)) {
+        std::cerr << "Not a door: " << d << std::endl;
+        exit(3);
+    }
+    return d + ('a' - 'A');
+}
+
+
+class Node {
+public:
+    Node(coord_type c): coords(c) {}
+    Node(coord_type c, key_type k): coords(c), required_key(k) {}
+
+    bool passable(const std::set<key_type> &keys) const {
+        if (required_key == 0) {
+            return true;
+        }
+        return keys.find(required_key) != keys.end();
+    }
+
+    const coord_type coords{0, 0};
+    auto distance = -1;
+    const key_type required_key = 0;
+}
+
+using grid_type = std::map<coord_type, std::shared_ptr<Node> >;
+
+
+std::vector<coord_type> get_neighbors(const grid_type &grid,
+                                      const coord_type &coords) {
+    std::vector<coord_type> neighbors;
+    for (auto &delta: {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}) {
+        coord_type possible_neighbor{coords[0] + delta[0], coords[1] + delta[1]};
+        if (grid.find(possible_neighbor) != grid.end()) {
+            neighbors.push_back(possible_neighbor);
+        }
+    }
+    return neighbors;
+}
+
+
+int main(int argc, char **argv) {
+    auto input_stream = open_input_file(argc, argv);
+
+    std::set<key_type> keys;
+
+    return 0;
+}
