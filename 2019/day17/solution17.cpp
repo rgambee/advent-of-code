cdf071cd0b0e3d150798f7e301f766766584e798
Robert Gambee <robertgambee@gmail.com>
2020-01-05 20:42:55 -0500

    Finish solution for day 17, part 2

--- 2019/day17/solution17.cpp
+++ 2019/day17/solution17.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <array>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <map>
 #include <memory>
+#include <sstream>
 #include <vector>
 
 #include "intcode.h"
@@ -56,6 +59,8 @@ public:
 using coord_type = std::array<size_t, 2>;
 using cell_ptr = std::shared_ptr<Cell>;
 using grid_type = std::map<coord_type, cell_ptr>;
+constexpr size_t NUM_MOVEMENT_FUNCTIONS = 3;
+constexpr size_t MAX_MOVEMENT_FUNCTION_LENGTH = 20;
 
 
 class Robot {
@@ -191,98 +196,239 @@ std::vector<Move> follow_scaffold(const grid_type &grid,
 }
 
 
-std::string compress(const std::vector<Move> &moves_vec,
-                     size_t max_length = 20,
-                     size_t num_movement_functions = 3) {
+std::string move_vector_to_string(std::vector<Move>::const_iterator start,
+                                  std::vector<Move>::const_iterator stop) {
     // Convert Move::FORWARDs to numerals
-    std::string moves_str;
+    std::stringstream moves_stream;
     auto forward_count = 0;
-    for (auto m: moves_vec) {
-        if (m == Move::LEFT || m == Move::RIGHT) {
+    for (; start != stop; ++start) {
+        if (*start == Move::LEFT || *start == Move::RIGHT) {
             if (forward_count > 0) {
-                moves_str.append(std::to_string(forward_count));
+                moves_stream << std::to_string(forward_count) << ',';
                 forward_count = 0;
             }
-            moves_str.push_back(static_cast<char>(m));
-        } else if (m == Move::FORWARD) {
+            moves_stream << static_cast<char>(*start) << ',';
+        } else if (*start == Move::FORWARD) {
             ++forward_count;
         }
     }
     if (forward_count > 0) {
-        moves_str.append(std::to_string(forward_count));
+        moves_stream << std::to_string(forward_count) << ',';
     }
-    std::cout << "Move string:" << std::endl;
-    std::cout << moves_str << std::endl;
+    auto result = moves_stream.str();
+    // Removing final trailing comma
+    result.erase(result.size() - 1, 1);
+    return result;
+}
 
-    // Compress the string into a main movement routine
-    // and movement functions
-    std::string function_names;
-    for (size_t func = 0; func < num_movement_functions; ++func) {
-        function_names.push_back(static_cast<char>(func) + 'A');
-        std::cout << function_names[function_names.size() - 1] << std::endl;
-        size_t best_compression = 0;
-        // size_t best_start = 0;
-        size_t best_len = 0;
-        std::string best_str;
-        for (size_t func_start = 0; func_start < moves_str.size(); ++func_start) {
-            // std::cout << "func_start: " << func_start << std::endl;
-            // Don't allow the move function to start mid-numeral
-            if (func_start > 0
-                && moves_str[func_start] != static_cast<char>(Move::LEFT)
-                && moves_str[func_start] != static_cast<char>(Move::RIGHT)) {
-                auto prev = moves_str[func_start - 1];
-                if (prev != static_cast<char>(Move::LEFT)
-                    && prev != static_cast<char>(Move::RIGHT)) {
-                    continue;
-                }
+
+std::vector<std::vector<size_t> > valid_move_functions(
+    const std::vector<Move> &moves_vec,
+    const std::vector<size_t> &func_starts,
+    const std::vector<size_t> &func_lengths,
+    size_t &failed_func,
+    size_t max_length = MAX_MOVEMENT_FUNCTION_LENGTH) {
+    // A valid set of move functions satisfies all of the following:
+    //      No overlapping
+    //      None longer than max_length characters
+    //      Fully describe the input move sequence
+    //      Main movement routine is not longer than max_length function calls
+    if (func_starts.size() != func_lengths.size()) {
+        std::cerr << "func_starts and func_lengths do not have the same size" << std::endl;
+        exit(5);
+    }
+    std::vector<size_t> func_ends(func_starts.size());
+    std::vector<std::string> func_strings(func_starts.size());
+    for (size_t i = 0; i < func_starts.size(); ++i) {
+        func_ends[i] = func_starts[i] + func_lengths[i];
+        if (func_ends[i] > moves_vec.size()) {
+            failed_func = i;
+            return {};
+        }
+        for (size_t j = 0; j < i; ++j) {
+            // Simple but incomplete overlap check
+            if ((func_starts[j] <= func_starts[i] && func_starts[i] < func_ends[j])
+                || (func_starts[j] < func_ends[i] && func_ends[i] <= func_ends[j])) {
+                failed_func = i;
+                return {};
             }
-            for (size_t func_len = 1; func_len <= max_length; ++func_len) {
-                // std::cout << "func_len: " << func_len << std::endl;
-                auto func_str = moves_str.substr(func_start, func_len);
-                // Don't allow the move function to end mid-numeral
-                auto func_end = func_start + func_len;
-                if (func_end < moves_str.size()
-                    && moves_str[func_end] != static_cast<char>(Move::LEFT)
-                    && moves_str[func_end] != static_cast<char>(Move::RIGHT)) {
-                    auto next = moves_str[func_end];
-                    if (next != static_cast<char>(Move::LEFT)
-                        && next != static_cast<char>(Move::RIGHT)) {
-                        continue;
+        }
+        func_strings[i] = move_vector_to_string(
+            std::next(moves_vec.cbegin(), func_starts[i]),
+            std::next(moves_vec.cbegin(), func_ends[i]));
+        if (func_strings[i].size() > max_length) {
+            // Move function is too long
+            failed_func = i;
+            return {};
+        }
+    }
+    std::vector<std::vector<size_t> > func_matches(func_starts.size());
+    for (size_t i = 0; i < func_starts.size(); ++i) {
+        auto func_begin = std::next(moves_vec.cbegin(), func_starts[i]);
+        auto func_end = std::next(moves_vec.cbegin(), func_ends[i]);
+        auto match_iter = std::search(moves_vec.cbegin(), moves_vec.cend(),
+                                      func_begin, func_end);
+        for (; match_iter != moves_vec.cend();
+             match_iter = std::search(std::next(match_iter, func_lengths[i]),
+                                      moves_vec.cend(),
+                                      func_begin, func_end)) {
+            size_t match_index = std::distance(moves_vec.cbegin(), match_iter);
+            auto match_end = match_index + func_lengths[i];
+            auto add_match = true;
+            std::vector<std::vector<size_t> > matches_to_remove(func_starts.size());
+            // Comprehensive overlap check
+            for (size_t j = 0; j < i; ++j) {
+                for (size_t k = 0; k < func_matches[j].size(); ++k) {
+                    auto other_match = func_matches[j][k];
+                    auto other_end = other_match + func_lengths[j];
+                    auto match_starts_within_other = (other_match <= match_index
+                                                      && match_index < other_end);
+                    auto match_ends_within_other = (other_match < match_end
+                                                    && match_end <= other_end);
+                    auto other_starts_within_match = (match_index <= other_match
+                                                      && other_match < match_end);
+                    auto other_ends_within_match = (match_index < other_end
+                                                    && other_end <= match_end);
+                    if (match_starts_within_other && match_ends_within_other) {
+                        // B is contained within A:
+                        //      AAAAAA
+                        //        BB
+                        // This isn't necessarily a problem,
+                        // but this shouldn't be counted as a match for B.
+                        add_match = false;
+                    } else if (other_starts_within_match && other_ends_within_match) {
+                        // A is contained within B:
+                        //        AA
+                        //      BBBBBB
+                        // This isn't necessarily a problem,
+                        // but A's match needs to be replaced with B's.
+                        matches_to_remove[j].push_back(k);
+                    } else if (match_starts_within_other || match_ends_within_other) {
+                        // This is a partial overlap:
+                        //      AAAAAA      OR      AAAAAA
+                        //         BBBBBB        BBBBBB
+                        // Mark i as the failed function
+                        // since making it longer won't resolve this.
+                        failed_func = i;
+                        return {};
                     }
                 }
-                // Don't include other move functions
-                if (func_str.find(function_names) != std::string::npos) {
-                    break;
-                }
-                size_t compression = 0;
-                // std::cout << "Checking for matches" << std::endl;
-                for (auto find_start = moves_str.find(func_str);
-                     find_start < moves_str.size();
-                     find_start = moves_str.find(func_str, find_start + 1)) {
-                    // std::cout << "Match at " << find_start << std::endl;
-                    compression += func_len - 1;
-                }
-                if (compression > best_compression) {
-                    best_compression = compression;
-                    // best_start = func_start;
-                    best_len = func_len;
-                    best_str = func_str;
+            }
+            if (add_match) {
+                func_matches[i].push_back(match_index);
+                for (size_t j = 0; j < matches_to_remove.size(); ++j) {
+                    for (int k = static_cast<int>(matches_to_remove[j].size()) - 1;
+                         k >= 0; --k) {
+                        func_matches[j].erase(std::next(func_matches[j].begin(),
+                                                        matches_to_remove[j][k]));
+                    }
                 }
             }
         }
-        std::cout << "Best move function is: " << best_str << std::endl;
+    }
 
-        // Replace instances of this move function with its letter
-        std::cout << "Compressing" << std::endl;
-        for (auto find_start = moves_str.find(best_str);
-             find_start < moves_str.size();
-             find_start = moves_str.find(best_str)) {
-            moves_str.replace(find_start, best_len, 1, function_names[func]);
-            // std::cout << moves_str << std::endl;
+    size_t combined_move_func_length = 0;
+    size_t num_func_calls = 0;
+    for (size_t i = 0; i < func_starts.size(); ++i) {
+        combined_move_func_length += func_lengths[i] * func_matches[i].size();
+        num_func_calls += func_matches[i].size();
+    }
+    // We've already checked for overlap, so to check whether the entire
+    // move sequence is covered, we can compare its length to the combined
+    // lengths of the movement functions.
+    if (combined_move_func_length != moves_vec.size()) {
+        return {};
+    }
+    // Check whether the main movement routine contains too many function calls
+    if (num_func_calls > max_length) {
+        return {};
+    }
+    return func_matches;
+}
+
+
+std::string create_main_movement_routine(
+            const std::vector<std::vector<size_t> > func_matches) {
+    auto index_to_char = [](size_t index) -> char {
+        return static_cast<char>(index) + 'A';
+    };
+
+    std::stringstream mmr_stream;
+    std::vector<size_t> curr_indices(func_matches.size(), 0);
+    while (true) {
+        auto func_to_add = std::numeric_limits<size_t>::max();
+        for (size_t i = 0; i < func_matches.size(); ++i) {
+            if (curr_indices[i] < func_matches[i].size()) {
+                if (func_to_add >= func_matches.size()
+                    || func_matches[func_to_add][curr_indices[func_to_add]]
+                       > func_matches[i][curr_indices[i]]) {
+                    func_to_add = i;
+                }
+            }
+        }
+        if (func_to_add > func_matches.size()) {
+            break;
+        }
+        mmr_stream << index_to_char(func_to_add) << ',';
+        ++curr_indices[func_to_add];
+    }
+    auto result = mmr_stream.str();
+    // Removing final trailing comma
+    result.erase(result.size() - 1, 1);
+    return result;
+}
+
+
+std::vector<std::string> compress(const std::vector<Move> &moves_vec) {
+    std::vector<size_t> func_starts(NUM_MOVEMENT_FUNCTIONS, 0);
+    std::vector<size_t> func_lengths(NUM_MOVEMENT_FUNCTIONS, 1);
+    std::vector<std::string> result;
+    while (true) {
+        size_t failed_func = NUM_MOVEMENT_FUNCTIONS + 1;
+        for (size_t i = 0; i < func_starts.size(); ++i) {
+            std::cout << func_starts[i] << ":" << func_lengths[i] << ", ";
+        }
+        std::cout << '\r';
+        auto func_matches = valid_move_functions(moves_vec, func_starts,
+                                                 func_lengths, failed_func);
+        if (func_matches.size() == func_starts.size()) {
+            std::cout << "Solution found!" << std::endl;
+            result.push_back(create_main_movement_routine(func_matches));
+            for (size_t i = 0; i < func_starts.size(); ++i) {
+                result.push_back(move_vector_to_string(
+                    std::next(moves_vec.cbegin(), func_starts[i]),
+                    std::next(moves_vec.cbegin(), func_starts[i] + func_lengths[i])));
+            }
+            return result;
+        }
+        // for (size_t i = 0; i < func_starts.size(); ++i) {
+        //     std::cout << func_starts[i] << " " << func_lengths[i] << std::endl;
+        // }
+        // std::cout << "Failed " << failed_func << std::endl;
+        for (size_t i = func_starts.size() - 1; true; --i) {
+            if (i != failed_func && func_starts[i] + func_lengths[i] < moves_vec.size()) {
+                ++func_lengths[i];
+                for (size_t j = i+1; j < func_starts.size(); ++j) {
+                    func_starts[j] = func_starts[i] + func_lengths[i];
+                    func_lengths[j] = 1;
+                }
+                break;
+            }
+            if (func_starts[i] < moves_vec.size() - 1) {
+                ++func_starts[i];
+                func_lengths[i] = 1;
+                for (size_t j = i+1; j < func_starts.size(); ++j) {
+                    func_starts[j] = func_starts[i] + func_lengths[i];
+                    func_lengths[j] = 1;
+                }
+                break;
+            }
+            if (i == 0) {
+                std::cout << "No solution possible" << std::endl;
+                return result;
+            }
         }
     }
-    std::cout << "Compressed string: " << moves_str << std::endl;
-    return moves_str;
 }
 
 
@@ -295,11 +441,11 @@ int main(int argc, char **argv) {
     size_t column_index = 0;
     Robot robo;
 
-    auto intcode_input = []() -> intcode_type {
+    auto part1_input = []() -> intcode_type {
         return 0;
     };
 
-    auto intcode_output = [&](intcode_type output) -> void {
+    auto part1_output = [&](intcode_type output) -> void {
         auto output_char = static_cast<char>(output);
         if (output_char == '\n') {
             column_index = 0;
@@ -310,19 +456,42 @@ int main(int argc, char **argv) {
         }
     };
 
-    run_intcode_program(program, intcode_input, intcode_output);
+    // Read grid and calculate alignment parameters
+    run_intcode_program(program, part1_input, part1_output);
     draw_grid(grid, robo);
     auto align_param = calculate_alignment_params(grid);
+
+    // Create path that follows scaffold and compress it
+    // into a main movement routine and several movement functions
     auto moves = follow_scaffold(grid, robo);
-    for (auto &move: moves) {
-        std::cout << static_cast<char>(move);
+    auto movement_routines = compress(moves);
+    for (auto &s: movement_routines) {
+        std::cout << s << std::endl;
     }
-    std::cout << std::endl;
-    compress(moves);
+
+    // Send robot movement instructions
+    std::stringstream robot_input;
+    for (size_t i = 0; i < movement_routines.size(); ++i) {
+        robot_input << movement_routines[i] << '\n';
+    }
+    // Don't show video feed
+    robot_input << "n\n";
+    auto part2_input = [&robot_input]() -> intcode_type {
+        return static_cast<intcode_type>(robot_input.get());
+    };
+
+    intcode_type dust_collected;
+    auto part2_output = [&dust_collected](intcode_type output) -> void {
+        dust_collected = output;
+    };
+    // Switch program mode
+    program[0] = 2;
+    run_intcode_program(program, part2_input, part2_output);
 
     std::cout << "PART 1" << std::endl;
     std::cout << "Alignment paramter: " << align_param << std::endl;
     std::cout << std::endl;
     std::cout << "PART 2" << std::endl;
+    std::cout << "Dust collected: " << dust_collected << std::endl;
     return 0;
 }
