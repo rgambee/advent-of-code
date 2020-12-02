f86151af1f352831d19e2ed1c86fca9c7b631220
Robert Gambee <robert@formlabs.com>
2020-12-02 07:50:23 -0500

    Add solution for day 2

---
+++ 2020/day02/solution02.js
@@ -0,0 +1,46 @@
+import {readFile, splitIntoLines} from '../util.js';
+export {main02};
+
+const LINE_REGEX = /^(?<min>[0-9]+)-(?<max>[0-9]+) (?<letter>[a-z]): (?<password>[a-z]+)$/;
+
+function main02(filePath) {
+    const lines = splitIntoLines(readFile(filePath));
+    let validCountA = 0;
+    let validCountB = 0;
+    for (let line of lines) {
+        const match = line.match(LINE_REGEX);
+        if (!match) {
+            continue;
+        }
+        if (isValidA(match.groups.min, match.groups.max,
+                     match.groups.letter, match.groups.password)) {
+            ++validCountA;
+        }
+        if (isValidB(match.groups.min, match.groups.max,
+                     match.groups.letter, match.groups.password)) {
+            ++validCountB;
+        }
+    }
+    return {'Part A': validCountA, 'Part B': validCountB};
+}
+
+function isValidA(minCount, maxCount, letter, password) {
+    let letterCount = 0;
+    const letterMatch = password.match(new RegExp(letter, 'g'));
+    if (letterMatch) {
+        letterCount = letterMatch.length;
+    }
+    return (minCount <= letterCount && letterCount <= maxCount);
+}
+
+function isValidB(firstIndex1, secondIndex1, letter, password) {
+    const firstIndex0 = firstIndex1 - 1;
+    const secondIndex0 = secondIndex1 - 1;
+    if (firstIndex0 < 0 || firstIndex0 >= password.length
+        || secondIndex0 < 0 || secondIndex0 >= password.length) {
+        return false;
+    }
+    const firstMatch = password[firstIndex0] === letter;
+    const secondMatch = password[secondIndex0] === letter;
+    return ((firstMatch || secondMatch) && !(firstMatch && secondMatch));
+}
--- 2020/solve.js
+++ 2020/solve.js
@@ -1,10 +1,16 @@
 'use strict';
 
 import {main01} from './day01/solution01.js';
+import {main02} from './day02/solution02.js';
 
 function solve01() {
     const result = main01('./day01/input01.txt');
     document.getElementById('solution01').innerHTML = JSON.stringify(result);
 }
-
 document.getElementById('button01').onclick = solve01;
+
+function solve02() {
+    const result = main02('./day02/input02.txt');
+    document.getElementById('solution02').innerHTML = JSON.stringify(result);
+}
+document.getElementById('button02').onclick = solve02;
