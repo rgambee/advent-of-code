aa42856bddc00f95091b9c7cc84001719b0828df
Robert Gambee <robert@formlabs.com>
2020-12-03 22:22:24 -0500

    Show star progress in favicon

--- 2020/solve.js
+++ 2020/solve.js
@@ -21,3 +21,33 @@ function solve03() {
     document.getElementById('solution03').innerHTML = JSON.stringify(result);
 }
 document.getElementById('button03').onclick = solve03;
+
+function generateFavicon(numStars) {
+    console.log('Generating favicon...');
+    const canvas = document.createElement('canvas');
+    canvas.width = 16;
+    canvas.height = 16;
+    // Use colors from AoC theme
+    const background = [10, 14, 37];
+    const foreground = [255, 255, 85];
+    const context = canvas.getContext('2d');
+    const imageData = context.createImageData(canvas.width, canvas.height);
+    for (let i = 0; i < canvas.width * canvas.height; ++i) {
+        let color = background;
+        if (numStars > 0 && (i % canvas.width) % 2
+            && (i % canvas.width) < canvas.width - 1
+            && (Math.floor(i / canvas.width) % 2)) {
+            // Fill in odd-numbered pixels as foreground
+            color = foreground;
+            --numStars;
+        }
+        [0, 1, 2].forEach(function(offset) {
+            imageData.data[i * 4 + offset] = color[offset];
+        });
+        imageData.data[i * 4 + 3] = 255;    // Alpha
+    }
+    context.putImageData(imageData, 0, 0);
+    console.log('Generated favicon');
+    return canvas.toDataURL();
+}
+document.getElementById('favicon-link').href = generateFavicon(6);
