145385f37f67e72e544d92987aac527924e418a8
Robert Gambee <robert@formlabs.com>
2020-12-06 07:43:21 -0500

    Add solution for day 6

---
+++ 2020/day06/solution06.js
@@ -0,0 +1,52 @@
+import {splitIntoLines} from '../util.js';
+export default main06;
+
+function main06(input) {
+    const lines = splitIntoLines(input, false);
+
+    let anyYes = new Set();
+    let allYes = null;
+    let anyCountA = 0;
+    let allCountB = 0;
+
+    for (let line of lines) {
+        if (line.length == 0) {
+            console.log('Counting answers', anyYes, allYes);
+            anyCountA += anyYes.size;
+            allCountB += allYes.size;
+            anyYes.clear();
+            allYes = null;
+        } else {
+            console.log('Adding answers', line);
+            anyYes = stringToSet(line, anyYes);
+            if (allYes === null) {
+                allYes = stringToSet(line);
+            } else {
+                allYes = intersection(allYes, stringToSet(line));
+            }
+        }
+    }
+
+    return {'Part A': anyCountA, 'Part B': allCountB};
+}
+
+function stringToSet(inputString, startingSet) {
+    let set = new Set();
+    if (startingSet !== undefined) {
+        set = new Set(startingSet);
+    }
+    for (let letter of inputString) {
+        set.add(letter);
+    }
+    return set;
+}
+
+function intersection(setA, setB) {
+    const intersection = new Set();
+    for (let item of setA) {
+        if (setB.has(item)) {
+            intersection.add(item);
+        }
+    }
+    return intersection;
+}
--- 2020/solve.js
+++ 2020/solve.js
@@ -2,7 +2,7 @@
 
 import {readFile} from './util.js';
 
-const DAY_NUMBER = 5;
+const DAY_NUMBER = 6;
 const NUM_STARS = DAY_NUMBER * 2;
 
 function main() {
