6d4552667df535eb220ef08af022160bbef5f504
Robert Gambee <robertgambee@gmail.com>
2021-12-22 21:24:11 -0500

    Finish solution for day 19

--- 2021/src/day19/solution19.rs
+++ 2021/src/day19/solution19.rs
@@ -1,25 +1,31 @@
 use crate::util;
 use regex::Regex;
 use std::cmp;
+use std::collections::VecDeque;
 use std::fs;
 use std::path;
 
 type RotationMatrix3D = ((i64, i64, i64), (i64, i64, i64), (i64, i64, i64));
 
+// rustfmt wants to format these matrices as single lines,
+// which I think is far less readable
+#[rustfmt::skip]
 const ROTATE_X: RotationMatrix3D = (
-    (1, 0, 0),
+    (1, 0,  0),
     (0, 0, -1),
-    (0, 1, 0),
+    (0, 1,  0),
 );
+#[rustfmt::skip]
 const ROTATE_Y: RotationMatrix3D = (
-    (0, 0, 1),
-    (0, 1, 0),
+    ( 0, 0, 1),
+    ( 0, 1, 0),
     (-1, 0, 0),
 );
+#[rustfmt::skip]
 const ROTATE_Z: RotationMatrix3D = (
     (0, -1, 0),
-    (1, 0, 0),
-    (0, 0, 1),
+    (1,  0, 0),
+    (0,  0, 1),
 );
 
 type Point3D = (i64, i64, i64);
@@ -36,26 +42,6 @@ fn rotate_point(point: &Point3D, rot: &RotationMatrix3D) -> Point3D {
     )
 }
 
-fn distance2(p1: &Point3D, p2: &Point3D) -> i64 {
-    // Square of distance to avoid taking root
-    let d0 = p1.0 - p2.0;
-    let d1 = p1.1 - p2.1;
-    let d2 = p1.2 - p2.2;
-    d0 * d0 + d1 * d1 + d2 * d2
-}
-
-fn calculate_centroid(points: &[Point3D]) -> Point3D {
-    if points.is_empty() {
-        panic!("Cannot calculate centroid of empty slice");
-    }
-    let mut centroid = (0, 0, 0);
-    for p in points.iter() {
-        centroid = translate_point(&centroid, p);
-    }
-    let n = points.len() as i64;
-    (centroid.0 / n, centroid.1 / n, centroid.2 / n)
-}
-
 fn translate_points(points: &[Point3D], translation: &Point3D) -> Vec<Point3D> {
     points
         .iter()
@@ -67,96 +53,48 @@ fn rotate_points(points: &[Point3D], rotation: &RotationMatrix3D) -> Vec<Point3D
     points.iter().map(|p| rotate_point(p, rotation)).collect()
 }
 
-fn center_points(points: &[Point3D]) -> Vec<Point3D> {
-    let centroid = calculate_centroid(points);
-    translate_points(points, &(-centroid.0, -centroid.1, -centroid.2))
-}
-
-fn calculate_error(points_a: &[Point3D], points_b: &[Point3D]) -> i64 {
-    let mut total_error = 0;
-    for pa in points_a.iter() {
-        let mut min_dist = i64::MAX;
-        for pb in points_b.iter() {
-            min_dist = cmp::min(distance2(pa, pb), min_dist);
-        }
-        total_error += min_dist;
-    }
-    total_error
-}
-
-fn find_nearest_point(point: &Point3D, points: &[Point3D]) -> Point3D {
-    let mut min_dist2 = i64::MAX;
-    let mut nearest_point = points[0];
-    for pt in points.iter() {
-        let dist2 = distance2(point, pt);
-        if dist2 < min_dist2 {
-            min_dist2 = dist2;
-            nearest_point = *pt;
-        }
-    }
-    nearest_point
-}
-
-fn align_points(reference: &[Point3D], to_align: &[Point3D]) -> Vec<Point3D> {
-    println!("Aligning points");
-    let mut selected_reference = to_align
-        .iter()
-        .map(|pt| find_nearest_point(pt, reference))
-        .collect::<Vec<Point3D>>();
-    selected_reference.sort_unstable();
-    selected_reference.dedup();
-
-    let centroid_ref = calculate_centroid(&selected_reference);
-    let centroid_other = calculate_centroid(to_align);
-    let centered_ref = translate_points(
-        &selected_reference,
-        &(-centroid_ref.0, -centroid_ref.1, -centroid_ref.2),
-    );
-    let mut centered_other = translate_points(
-        to_align,
-        &(-centroid_other.0, -centroid_other.1, -centroid_other.2),
-    );
-
-    let mut min_error = calculate_error(&centered_ref, &centered_other);
-    println!("Unroated error: {}", min_error);
-    let mut best_other = centered_other.clone();
-    let mut rotated = false;
-    // This is redundant since it checks some rotations multiple times
-    for _i in 0..4 {
-        centered_other = rotate_points(&centered_other, &ROTATE_X);
-        for _j in 0..4 {
-            centered_other = rotate_points(&centered_other, &ROTATE_Y);
-            for _k in 0..4 {
-                centered_other = rotate_points(&centered_other, &ROTATE_Z);
-                let error = calculate_error(&centered_ref, &centered_other);
-                // println!("Error: {}", error);
-                if error < min_error {
-                    min_error = error;
-                    best_other = centered_other.clone();
-                    rotated = true;
+fn align_points(reference: &[Point3D], to_align: &[Point3D]) -> Option<(Vec<Point3D>, Point3D)> {
+    let mut reference_copy = Vec::from(reference);
+    reference_copy.sort_unstable();
+    let mut to_align_copy = Vec::from(to_align);
+    to_align_copy.sort_unstable();
+    for ref_point in reference_copy.iter() {
+        let ref_negated = (-ref_point.0, -ref_point.1, -ref_point.2);
+        for other_point in to_align_copy.iter() {
+            let mut other_copy = *other_point;
+            let other_negated = (-other_point.0, -other_point.1, -other_point.2);
+            let mut transformed_other = translate_points(&to_align_copy, &other_negated);
+            // This is redundant since it checks some rotations multiple times
+            for outer_matrix in [ROTATE_X, ROTATE_Y].iter() {
+                for _i in 0..4 {
+                    for _j in 0..4 {
+                        transformed_other = translate_points(&transformed_other, ref_point);
+                        let match_count = transformed_other
+                            .iter()
+                            .filter(|p| reference_copy.binary_search(p).is_ok())
+                            .count();
+                        if match_count >= 12 {
+                            let other_origin = translate_point(
+                                ref_point,
+                                &(-other_copy.0, -other_copy.1, -other_copy.2),
+                            );
+                            let mut combined_points = reference_copy;
+                            combined_points.append(&mut transformed_other);
+                            combined_points.sort_unstable();
+                            combined_points.dedup();
+                            return Some((combined_points, other_origin));
+                        }
+                        transformed_other = translate_points(&transformed_other, &ref_negated);
+                        transformed_other = rotate_points(&transformed_other, &ROTATE_Z);
+                        other_copy = rotate_point(&other_copy, &ROTATE_Z);
+                    }
+                    transformed_other = rotate_points(&transformed_other, outer_matrix);
+                    other_copy = rotate_point(&other_copy, outer_matrix);
                 }
             }
         }
     }
-    if rotated {
-        // Try another iteration until it converges
-        println!("Recursing");
-        return align_points(reference, &best_other);
-    }
-    // Combine points and remove duplicates
-    best_other = translate_points(&best_other, &centroid_ref);
-    let mut combined = Vec::from(reference);
-    combined.append(&mut best_other);
-    combined.sort_unstable();
-    combined.dedup();
-    println!(
-        "Originally {} and {} beacons, now {} (change of {})",
-        reference.len(),
-        to_align.len(),
-        combined.len(),
-        reference.len() + to_align.len() - combined.len(),
-    );
-    combined
+    None
 }
 
 pub fn solve(input_path: path::PathBuf) -> util::Solution {
@@ -165,13 +103,13 @@ pub fn solve(input_path: path::PathBuf) -> util::Solution {
 
     let contents = fs::read_to_string(&input_path)
         .unwrap_or_else(|_| panic!("Failed to read input file {:?}", input_path));
-    let mut all_beacons: Vec<Vec<Point3D>> = Vec::new();
+    let mut all_beacons: VecDeque<Vec<Point3D>> = VecDeque::new();
     let mut beacons: Vec<Point3D> = Vec::new();
     let mut beacon_count: i64 = 0;
     for line in contents.lines() {
         if scanner_regex.is_match(line) {
             if !beacons.is_empty() {
-                all_beacons.push(beacons);
+                all_beacons.push_back(beacons);
                 beacons = Vec::new();
             }
         } else if let Some(captures) = beacon_regex.captures(line) {
@@ -186,7 +124,7 @@ pub fn solve(input_path: path::PathBuf) -> util::Solution {
         }
     }
     if !beacons.is_empty() {
-        all_beacons.push(beacons);
+        all_beacons.push_back(beacons);
     }
     println!(
         "Found {} scanners and {} beacons",
@@ -194,15 +132,37 @@ pub fn solve(input_path: path::PathBuf) -> util::Solution {
         beacon_count,
     );
 
-    let mut beacon_iter = all_beacons.into_iter();
-    let first_set = beacon_iter.next().unwrap();
-    let aligned = beacon_iter.fold(first_set, |algn, pts| align_points(&algn, &pts));
+    let mut aligned_beacons = all_beacons.pop_front().expect("No beacons found");
+    let mut origins: Vec<Point3D> = vec![(0, 0, 0)];
+    while !all_beacons.is_empty() {
+        println!("{} scanners left to align", all_beacons.len());
+        let to_align = all_beacons.pop_front().unwrap();
+        if let Some((aligned, other_origin)) = align_points(&aligned_beacons, &to_align) {
+            aligned_beacons = aligned;
+            origins.push(other_origin);
+        } else {
+            println!("Unable to align scanner, skipping for now");
+            all_beacons.push_back(to_align);
+        }
+    }
+
+    let mut max_distance = 0;
+    for (i, org1) in origins.iter().enumerate() {
+        for org2 in origins[i + 1..].iter() {
+            let distance =
+                (org1.0 - org2.0).abs() + (org1.1 - org2.1).abs() + (org1.2 - org2.2).abs();
+            max_distance = cmp::max(max_distance, distance);
+        }
+    }
 
     util::Solution(
         Some(util::PartialSolution {
             message: String::from("Number of beacons"),
-            answer: aligned.len() as i64,
+            answer: aligned_beacons.len() as i64,
         }),
-        None,
+        Some(util::PartialSolution {
+            message: String::from("Maximum distance between scanners"),
+            answer: max_distance,
+        }),
     )
 }
